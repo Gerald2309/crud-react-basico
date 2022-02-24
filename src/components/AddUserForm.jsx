fbc75e5fe550aa916f7a2e7db65cd5f50dd70de6
Gerald2309 <68652749+Gerald2309@users.noreply.github.com>
2022-02-23 20:35:04 -0500

    Añadir usuario (con validación)

--- src/components/AddUserForm.jsx
+++ src/components/AddUserForm.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
 
+
 const AddUserForm = (props) => {
-  
-  const { register, errors, handleSubmit } = useForm();
+
+  const { register, formState: { errors }, handleSubmit } = useForm();
 
   const onSubmit = (data) => {
     console.log(data)
@@ -12,9 +13,22 @@ const AddUserForm = (props) => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <label>Name</label>
-      <input type="text" name="name"  />
+      <input type="text" {...register('name', { required: true })} />
+      <div>
+        <span className='text-danger text-small d-block mb-2'>
+          {errors.name && "Este campo es requerido"}
+        </span>
+
+      </div>
+
       <label>Username</label>
-      <input type="text" name="username" />
+      <input type="text" {...register('username', { required: true })} />
+      <div>
+        <span className='text-danger text-small d-block mb-2'>
+          {errors.username && "Este campo es requerido"}
+        </span>
+
+      </div>
       <button>Add new user</button>
     </form>
   );
